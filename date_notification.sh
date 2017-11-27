a820585f3028cb325587830cf90e19013da162c1
cmolinaord <carlosmolina.ord@gmail.com>
2017-11-27 11:09:00 +0100

    Change uptime to pretty format and message to english

--- date_notification.sh
+++ date_notification.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-notify-send --icon=/home/cmolina/.icons/clock.png "Son las $(date +"%H:%M")" "uptime: $(uptime |cut -d" " -f5|sed "s/,//")"
+notify-send --icon=/home/cmolina/.icons/clock.png "Now it's $(date +"%H:%M")" "$(uptime -p)"
